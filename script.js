4182a8c97a656eb2cb490b6c131d9ebddc21fb7b
rperez1020 <49043860+rperez1020@users.noreply.github.com>
2023-12-13 00:04:14 -0800

    update

--- script.js
+++ script.js
@@ -4,16 +4,30 @@ const list = document.querySelector('#list')
 const template = document.querySelector("#list-item-template")
 const LOCAL_STORAGE_PREFIX = "ADVANCED_TODO_LIST"
 const TODOS_STORAGE_KEY = `${LOCAL_STORAGE_PREFIX}-todos`
-const todos = loadTodos()
+let todos = loadTodos()
 todos.forEach(renderTodo)
 
 
 list.addEventListener('change', e => {
     if(!e.target.matches('[data-list-item-checkbox]')) return
-        
+    const parent = e.target.closest('.list-item')
+    const todoId = parent.dataset.todoId
+    const todo = todos.find(t => t.id === todoId)
+    todo.complete = e.target.checked
+    saveTodos()
     
 })
 
+list.addEventListener('click', e =>{
+    if(!e.target.matches('[data-button-delete'))return
+
+    const parent = e.target.closes('.list-item')
+    const todoId = parent.dataset.todoId
+
+    parent.remove()
+    todos = todos.filter(todo => todo.id !== todoId)
+    saveTodos()
+})
 
 form.addEventListener('submit', e =>{
     e.preventDefault()
@@ -22,7 +36,8 @@ form.addEventListener('submit', e =>{
     if(todoName === "") return
     const newTodo = {
         name: todoName,
-        complete: false
+        complete: false,
+        id: new Date().valueOf().toString()
     }
 
     todos.push(newTodo)
@@ -33,8 +48,12 @@ form.addEventListener('submit', e =>{
 
 function renderTodo(todo){
     const templateClone = template.content.cloneNode(true)
+    const listItem = templateClone.querySelector('.list-item')
+    listItem.dataset.todoId = todo.id
     const textElement = templateClone.querySelector('[data-list-item-text]')
     textElement.innerText = todo.name
+    const checkbox = templateClone.querySelector("[data-list-item-checkbox")
+    checkbox.checked = todo.complete
     list.appendChild(templateClone)
 }
 
